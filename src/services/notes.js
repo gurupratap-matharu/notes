bdbce85e71b679ae85cd52c1a10ce4c4d8fd6f8c
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-07-01 19:47:40 -0300

    added relative baseUrl

--- src/services/notes.js
+++ src/services/notes.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'https://gurupratap-notes-backend.herokuapp.com/api/notes'
+const baseUrl = '/api/notes'
 
 const getAll = () => {
     const request = axios.get(baseUrl)
