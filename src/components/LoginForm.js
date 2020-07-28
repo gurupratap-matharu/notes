94e091cd5cf828bfd06c2c6d3f310d6160efa68e
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-07-28 18:42:56 -0300

    added proptypes definitions for handler functions

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,26 +1,43 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-const LoginForm = (props) => (
-    <form onSubmit={props.handleLogin}>
-        <div>
-            username
+const LoginForm = ({
+    handleLogin,
+    handleUsernameChange,
+    handlePasswordChange,
+    userName,
+    password
+}) => {
+    return (
+        <form onSubmit={handleLogin}>
+            <div>
+                username
              <input
-                type="text"
-                name="Username"
-                value={props.userName}
-                onChange={props.handleUsernameChange}
-            />
-        </div>
-        <div>
-            password
+                    type="text"
+                    name="Username"
+                    value={userName}
+                    onChange={handleUsernameChange}
+                />
+            </div>
+            <div>
+                password
              <input
-                type="password"
-                name="Password"
-                value={props.password}
-                onChange={props.handlePasswordChange}
-            />
-        </div>
-        <button type="submit">Login</button>
-    </form>
-)
+                    type="password"
+                    name="Password"
+                    value={password}
+                    onChange={handlePasswordChange}
+                />
+            </div>
+            <button type="submit">Login</button>
+        </form>
+    )
+}
+LoginForm.propTypes = {
+    handleLogin: PropTypes.func.isRequired,
+    handleUsernameChange: PropTypes.func.isRequired,
+    handlePasswordChange: PropTypes.func.isRequired,
+    username: PropTypes.string.isRequired,
+    password: PropTypes.string.isRequired
+
+}
 export default LoginForm
